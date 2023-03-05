b18a3f40ba76d59062fa558a4b42fa5061aa0d16
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-06 02:56:25 +0300

    test navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Navbar(){
+    return(
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <p>HEy</p>
+          </div>
+        </nav>
+    )
+}
+
+export default Navbar;
