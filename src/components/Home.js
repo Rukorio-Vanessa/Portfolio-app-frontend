ab5b0f276f4d0851c9dd23e7bfad3a041faeb0f9
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-06 09:26:50 +0300

    deleted first login trial

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ function Home(){
         <h2 className="heading-2"> Please Log In to Proceed </h2>
 
         <p>
-          MyPortfolio offers a quick and easy way to handle your projects and skills
+          MyPortfolio offers a quick and easy way for you to handle your projects and skills
         </p>
 
         </div>
