ede87622fe2dbc763b12924eb2b2782702efc5b7
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-06 04:37:35 +0300

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Home from './components/Home';
+import { Route, Switch } from "react-router-dom";
+import Login from './components/Login'
+
 
 function App() {
   return (
-    <Navbar/>
-  )
+    <div className="App">
+      
+    <Switch>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      <Route path='/'>
+        <Home />
+      </Route>
+    </Switch>
+    
+    </div>
+  );
 }
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+
 function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <p>HEy</p>
+
+            <h3 className="navbar-brand">MY PORTFOLIO</h3>
+            <NavLink to="/">HOME</NavLink> 
+            <NavLink to="/login">LOG IN</NavLink> 
+            
           </div>
         </nav>
     )
